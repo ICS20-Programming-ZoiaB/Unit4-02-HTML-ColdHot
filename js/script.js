0183f5590f65f43b7092620e8bbded9b6e9b14a7
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-07 18:30:39 +0000

    Added code

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2023 ZoiaB All rights reserved
+//
+// Created by: ZoiaB
+// Created on: Apr 2023
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function tempSent() {
+
+ //declaring original variables & constant
+ let temperature = parseFloat(document.getElementById("temp-input").value);
+ const TEMP_THRESHOLD = 15;
+ let message = "";
+
+ //setting different if/then scenarios
+ if (temperature >= TEMP_THRESHOLD) {
+   message = "This is considered hot. Prepare for warmer weather!";
+ }
+
+ //else represents a temperature of less than 15 degrees
+ else {
+   message = "This is considered cold. Prepare for cooler weather!";
+ }
+
+ //display the correct message to the screen
+ document.getElementById("results").innerHTML = message;
+}
